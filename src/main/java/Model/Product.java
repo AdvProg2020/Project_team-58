47b0742aee86e1c2f0cd49c24a08f6f1ed81b251
Ali-Dehghani <63192787+Ali-Dehghani@users.noreply.github.com>
2020-05-04 15:41:08 +0430

    adding a view class

--- src/main/java/Model/Product.java
+++ src/main/java/Model/Product.java
@@ -1,4 +1,18 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Product {
+
+    private String id;
+    private String currentCondition;
+    private HashMap<String,String> commonAttributes;
+    private Category category;
+    private HashMap<String,String> specificAttributes;
+    private String description;
+    private float averageScore;
+    private ArrayList<Comment> Comments;
+    private int quantity;
 }
